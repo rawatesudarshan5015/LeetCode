8cfb34f6979114dab53b4b1a40aea2f6cdfc0ec9
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2025-10-14 15:40:05 +0530

    Time: 31 ms (9.54%), Space: 118.2 MB (7.36%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
 
-    int solve(int i,int rem,vector<int>&nums){
+
+
+    int solve(int i,int rem,vector<int>&nums,vector<vector<int>>&dp){
         if(i>=nums.size()) return 0;
         if(i==nums.size()-1){
             return 1;
         }
 
-        int ans=1;
+       int ans = 1;
         
-    
+        if(dp[rem][i]!=-1){
+           return dp[rem][i];
+        }
             
            for(int j = i+1;j<nums.size();j++){
             if((nums[i]+nums[j])%2==rem){
-               
-                ans =ans+ solve(j,rem,nums);
+                ans = ans+ solve(j,rem,nums,dp);
                 break;
             }
            
 
         }
-        return ans;
+        return dp[rem][i]=ans;
 
     }
     int maximumLength(vector<int>& nums) {
         int temp = nums[0]%2;
         int idx = 0;
+        vector<vector<int>>dp(2,(vector<int>(nums.size()+1,-1)));
         for(int i=1;i<nums.size();i++){
             if(temp!=nums[i]%2){
                 idx = i;
@@ -33,10 +37,10 @@ public:
             }
         }
         int ans =0;
-        ans= solve(0,0,nums);
-        ans = max(ans,solve(idx,0,nums));
-        ans = max(ans,solve(0,1,nums));
-        ans = max(ans,solve(idx,1,nums));
+        ans= solve(0,0,nums,dp);
+        ans = max(ans,solve(0,1,nums,dp));
+        ans = max(ans,solve(idx,0,nums,dp));
+        ans = max(ans,solve(idx,1,nums,dp));
         return ans;
 
     }
