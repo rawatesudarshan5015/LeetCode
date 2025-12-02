93c505312c315c5061425aaf80a240a45ad2c7d8
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2025-12-02 09:18:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+
+        int element = nums[0];
+        int count =0;
+
+        for(int i=0;i<nums.size();i++){
+            if(element==nums[i]){
+                count++;
+            }else{
+                count--;
+                if(count==0) element = nums[i]; count++;
+            }
+        }
+        return element;
+    }
+};
