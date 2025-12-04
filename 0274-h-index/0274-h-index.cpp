26171a480efe587c439fecb936dc8933532c07a9
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2025-12-04 10:19:30 +0530

    Time: 5 ms (6.92%), Space: 11.2 MB (100%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int n = citations.size();
+        int i;
+        for(i=1;i<=n;i++){
+            if(citations[n-i]<i) break;
+        }
+        return i-1;
+    }
+};
