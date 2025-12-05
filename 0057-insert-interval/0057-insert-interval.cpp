21adfbe97c5c1002b5f65c3ad9605cc534343679
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2025-12-05 09:43:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,29 +1,64 @@
+// class Solution {
+// public:
+//     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+//         // int n = intervals.size();
+//         // vector<vector<int>>ans;
+//         // int i = 0;
+
+//         // while(i<n && newInterval[0]>intervals[i][1]){
+//         //     ans.push_back(intervals[i]);
+//         //     i++;
+//         // }
+
+//         // while(i<n && newInterval[1]>=intervals[i][0]){
+//         //     newInterval[0]= min(newInterval[0],intervals[i][0]);
+//         //     newInterval[1] = max(newInterval[1],intervals[i][1]);
+//         //     i++;
+//         // }
+//         // ans.push_back(newInterval);
+
+
+//         // while(i<n){
+//         //     ans.push_back(intervals[i]);
+//         //     i++;
+//         // }
+
+//         // return ans;
+
+
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        int n = intervals.size();
+
         vector<vector<int>>ans;
-        int i = 0;
 
-        while(i<n && newInterval[0]>intervals[i][1]){
-            ans.push_back(intervals[i]);
-            i++;
+        for(int i=0;i<intervals.size();i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int newStart;
+            int newEnd;
+            if(newInterval[0]>=start && newInterval[0]<=end){
+            
+                newStart = min(newInterval[0],start);
+
+                for(;i<intervals.size();i++){
+                    if(newInterval[1]>=intervals[i][0] && newInterval[1]<=intervals[i][1]){
+                        newEnd = intervals[i][1];
+                        ans.push_back({newStart,newEnd});
+                        break;
+                    }
+                    i++;
+                } 
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
         }
-
-        while(i<n && newInterval[1]>=intervals[i][0]){
-            newInterval[0]= min(newInterval[0],intervals[i][0]);
-            newInterval[1] = max(newInterval[1],intervals[i][1]);
-            i++;
-        }
-        ans.push_back(newInterval);
-
-
-        while(i<n){
-            ans.push_back(intervals[i]);
-            i++;
-        }
-
         return ans;
     }
 };
