9c9799f4c3229a8ee9c09b4284570b175622a859
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2026-01-12 12:23:29 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.99%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        else if(n==1) return 1;
+
+        int first = 0;
+        int second = 1;
+        
+        int count = 2;
+
+        while(count<=n){
+            int temp = first+second;
+            first = second;
+            second = temp;
+            count++;
+        }
+        return second;
+        
+    }
+};
